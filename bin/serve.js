91a669759141a7088afda196eb666244483bb979
Matt Glazar <matt.glazar@gmail.com>
2011-07-03 16:21:01 -0700

    Better debug outputting of requests/responses

--- bin/serve.js
+++ bin/serve.js
@@ -1,5 +1,6 @@
 var static = require('node-static');
 var http = require('http');
+var sys = require('sys');
 
 var port = 80;
 var basePath = process.cwd();
@@ -11,9 +12,25 @@ basePath = args.pop() || basePath;
 
 var fileServer = new static.Server(basePath);
 
-http.createServer(function (request, response) {
-    request.addListener('end', function () {
-        fileServer.serve(request, response);
+http.createServer(function (req, res) {
+    req.addListener('end', function () {
+        fileServer.serve(req, res, function (err, serveRes) {
+            if (err) {
+                // An error as occured
+                sys.error('Error serving ' + req.url + ': ' + err.message);
+                res.writeHead(err.status, err.headers);
+                res.end();
+            } else {
+                // The file was served successfully
+                var prefix = ' - ';
+                var headers = res._header.split('\r\n').slice(1).filter(function (header) {
+                    return !!header;
+                });
+
+                sys.puts(req.method + ' ' + req.url + ' [' + serveRes.status + ' ' + serveRes.message + ']');
+                sys.puts(prefix + headers.join('\n' + prefix));
+            }
+        });
     });
 }).listen(port);
 
